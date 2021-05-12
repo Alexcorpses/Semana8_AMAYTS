531c402fc340359b7dd41a47ecdf711c9faab38b
Alexcorpses <46629758+Alexcorpses@users.noreply.github.com>
2021-05-11 19:25:22 -0600

    java form completed
    java form, using local memory

---
+++ src/clasesemana8/ClaseSemana8.java
@@ -0,0 +1,22 @@
+
+package clasesemana8;
+import vistas.RegProductos;
+
+
+/**
+ *
+ * @author Alex
+ */
+public class ClaseSemana8 {
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        RegProductos vista = new RegProductos();
+        
+        vista.setVisible(true);
+    }
+    
+}
